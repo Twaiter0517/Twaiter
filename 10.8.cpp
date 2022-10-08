2d4e477c74471293edc01f61009c70f0c6d67b5c
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-08 17:19:53 +0800

    Add files via upload

---
+++ 10.8.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+
+//int main()
+//{
+	//char arrl[]="abc";
+	//char arr2[]={'a','b','c'};
+	//printf("%s\n",arr2);
+//return 0;
+//
+//}
+
+//#include<stdio.h>
+
+//int main()
+//{
+//	char arr1[]="abc";
+//		char arr2[]={'a','b','c',0};
+//	printf("%s\n",arr1);
+//printf("%s\n",arr2);
+//return 0;
+//}
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+	//char arr1[]="abc";
+	//char arr2[]={'a','b','c'};
+	//printf("%s\n",arr1);
+	//printf("%s\n",arr2);
+	//printf("%d\n",strlen(arr1));
+	//printf("%d\n",strlen(arr2));
+//return 0;
+//}	
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("c:\\test\\32\\test.c");
+//return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+	//printf("%d\n",strlen("c:\test\32\test.c"));
+//return 0;
+//}
+/*
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	printf("%d\n",strlen("abcdef"));
+	printf("%d\n",strlen("c;\test\328\test.c"));
+return 0;
+}
+*/
