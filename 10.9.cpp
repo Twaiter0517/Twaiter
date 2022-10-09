d6c11bce7fe20394e29ef292e3cd74084f1cd855
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-09 23:58:54 +0800

    Add files via upload

---
+++ 10.9.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	int input=0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？(1/0)>:");
+//	scanf("%d",&input);
+//	if(input==1)
+//	printf("拿到好offer\n");
+//	else
+//	printf("回家卖红薯\n");
+//
+//return 0;
+//}
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int line=0;
+//	printf("加入比特\n");
+//
+//	while(line<20000)
+//	{
+//printf("敲代码~~~~~:%d\n",line);
+//line++;
+//	}
+//	if(line>=20000)
+//	printf("拿到好offer 迎娶白富美走上人生巅峰\n");
+//return 0;
+//}
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int line=0;
+//	printf("加入比特\n");
+//
+//	while(line<20000)
+//	{
+//	printf("敲代码~~~：%d\n",line);
+//	line++;
+//	}
+//	if(line>=20000)
+//		printf("拿到好offer\n");
+//
+//return 0;
+//}
+
+//
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？(1/0)>:\n");
+//	int input=0;
+//	scanf("%d",&input);
+//	if(input==1)
+//		printf("拿到好offer\n");
+//	else
+//	printf("回家卖红薯\n");
+//
+//
+//return 0;
+//}
