409b6beba3be870210fbf375730d9d5a411b9a9a
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-10 20:08:30 +0800

    Add files via upload

---
+++ 10.10.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//
+//{
+//	char arr1[]="abc";
+//		char arr2[]={'a','b','c',0};
+//		printf("%s\n",arr1);
+//			printf("%s\n",arr2);
+//			printf("%d\n",strlen(arr1));
+//			printf("%d\n",strlen(arr2));
+//
+//return 0;
+//}
+//int Add(int x,int y)
+//{
+//	int z=x+y;
+//	return z;
+//}
+//
+//#include<stdio.h>
+//int main()
+//	
+//{
+//	int sum=0;
+//	int sum2=0;
+//	sum=Add(2,3);
+//	sum2=Add(3,4);
+//	printf("%d\n",sum);
+//	printf("%d\n",sum2);
+//
+//
+//return 0;
+//}
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10]={1,2,3,4,5,6,7,8,9,10};
+//	int i=0;
+//	while(i<10)
+//	{
+//		printf("%d	",arr[i]);
+//		i++;
+//	}
+//
+//return 0;
+//}
+
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int line=0;
+//	printf("加入比特\n");
+//	while(line<30000)
+//	{
+//	printf("敲代码~~~:%d\n",line);
+//	line++;
+//	}
+//	printf("拿到好offer\n");
+//
+//
+//return 0;
+//}
