ff89ceaef8bb8285438786ad8a962a4d320853f3
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-11 21:30:56 +0800

    Add files via upload

---
+++ 10.11.cpp
@@ -0,0 +1,93 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	printf("炙热沙城2中时赛\n");
+//	printf("打不打A？(1/0)\n");
+//		int input=0;
+//	scanf("%d",&input);
+//	if(input==1)
+//		printf("起大狙 给我爆闪抢A大!\n");
+//	else
+//		printf("起烟吹风机 rush他们B!\n");
+//
+//return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	int line=0;
+//	while(line<20000)
+//	{
+//	printf("敲代码~~~~%d\n",line);
+//	line++;
+//	
+//	}
+//	printf("拿到好offer");
+//
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("想要成为爆头哥？");
+//	int hour=0;
+//	while(hour<30000)
+//	{
+//	printf("练急停 练爆头线 压弹道%d\n",hour);
+//	hour++;
+//	
+//	}
+//	printf("开锁开锁!一枪一个头!\n");
+//return 0;
+//}
+//
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[]="abc";
+//	char arr2[]={97,'b','c',0};
+//	printf("%s\n",arr1);
+//	printf("%s\n",arr2);
+//	printf("%d\n",strlen(arr1));
+//	printf("%d\n",strlen(arr2));
+//		
+//
+//
+//return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(char));
+//	printf("%d\n",sizeof(short));
+//	printf("%d\n",sizeof(int));
+//	printf("%d\n",sizeof(long));
+//	printf("%d\n",sizeof(long long));
+//	printf("%d\n",sizeof(float));
+//	printf("%d\n",sizeof(double));
+//
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？\n");
+//	printf("你的选择是(1/0)\n");
+//	int input=0;
+//	scanf("%d",&input);
+//	if(input==1)
+//		printf("拿到好offer\n");
+//	else
+//		printf("回家卖红薯\n");
+//
+//
+//return 0;
+//}
