837a6012e5caa09dbaa5a309599d8be6d2bdfee7
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-12 19:00:14 +0800

    Add files via upload

---
+++ 10.12.cpp
@@ -0,0 +1,88 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[]="abc";
+//	char arr2[]={97,'b','c',0};
+//	printf("%s\n",arr1);
+//	printf("%s\n",arr2);
+//	printf("%d\n",strlen(arr1));
+//	printf("%d\n",strlen(arr2));
+//		
+//
+//
+//return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(char));
+//	printf("%d\n",sizeof(short));
+//	printf("%d\n",sizeof(int));
+//	printf("%d\n",sizeof(long));
+//	printf("%d\n",sizeof(long long));
+//	printf("%d\n",sizeof(float));
+//	printf("%d\n",sizeof(double));
+//
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？\n");
+//	printf("你的选择是(1/0)\n");
+//	int input=0;
+//	scanf("%d",&input);
+//	if(input==1)
+//		printf("拿到好offer\n");
+//	else
+//		printf("回家卖红薯\n");
+//
+//
+//return 0;
+//}
+
+//#i/*nclude<stdio.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	int line=0;
+//	while(line<30000)
+//	{
+//	printf("敲代码~~~%d\n",line);
+//	line++;
+//	}
+//	printf("拿到好offer\n");
+//
+//return 0;
+//}*/
+
+//int Add(int x,int y)
+//	{
+//		int z=x+y;
+//		return z;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int sum=0;
+//	sum=Add(2,3);
+//	printf("%d\n",sum);
+//return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	char arr[10]={1,2,3,4,5,6,7,8,9,10};
+//	int i=0;
+//	while(i<10)
+//	{
+//		printf("%d\n",i);
+//	i++;
+//	}
+//return 0;
+//}
