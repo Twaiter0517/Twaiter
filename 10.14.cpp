83c444140c13a3472c27c05bc8aafc12182c6b69
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-14 22:07:20 +0800

    Add files via upload

---
+++ 10.14.cpp
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int Add(int x,int y)
+//	{
+//		int z=x+y;
+//		return z;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int sum=0;
+//	sum=Add(2,3);
+//	printf("%d\n",sum);
+//return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	char arr[10]={1,2,3,4,5,6,7,8,9,10};
+//	int i=0;
+//	while(i<10)
+//	{
+//		printf("%d\n",i);
+//	i++;
+//	}
+//return 0;
+//}
+//#include<stdio.h>
+//int Max(int x,int y)
+//{
+//if(x>y)
+//	return x;
+//else
+//	return y;
+//
+//}
+//
+//int main()
+//{
+//	int num1=30;
+//	int num2=20;
+//	int max=0;
+//	max=Max(num1,num2);
+//	printf("%d\n",max);
+//return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int num1=10;
+//	int num2=20;
+//	if(num1>20)
+//		printf("%d\n",num1);
+//	else
+//		printf("%d\n",num2);
+//
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//		int a=0;
+//		int b=0;
+//		//int c=a&&b;       
+//		//a&&b逻辑与（并）
+//		//a||b逻辑或（或）
+//		int c=a||b;		
+//			printf("%d\n",c);
+//
+//return 0;
+//}
+
+//
+//int Add(int x,int y)
+//{
+//int z=x+y;
+//return z;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int sum=0;
+//	sum=Add(4,5);
+//	printf("%d\n",sum);
+//
+//
+//return 0;
+//}
