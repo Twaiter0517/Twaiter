9cd46a41a075ad9cd8fb917861854ce6690c9454
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-10-15 19:57:49 +0800

    Add files via upload

---
+++ 10.15.cpp
@@ -0,0 +1,108 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+//int Add(int x,int y)
+//{
+//int z=x+y;
+//return z;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int sum=0;
+//	sum=Add(4,5);
+//	printf("%d\n",sum);
+//
+//
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char arr[10]={0,1,2,3,4,5,6,7,8,9};
+//	int i=0;
+//	while(i<9)
+//	{
+//	printf("%d\n",i);
+//	i++;
+//	}
+//
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	int line=0;
+//	while(line<20000)
+//	{
+//	printf("敲代码%d\n",line);
+//	line++;
+//	
+//	}
+//	printf("get a good one\n");
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？\n");
+//	printf("你的选择是(1/0)\n");
+//	int input=0;
+//	scanf("%d",&input);
+//	if(input==1)
+//		printf("拿到好offer\n");
+//	else
+//		printf("sell potato\n");
+
+//return 0;
+//}
+//#include<stdio.h>
+//int Max(int x,int y)
+//{
+//if(x>y)
+//	return x;
+//else
+//	return y;
+//
+//}
+//
+//
+//int main()
+//{
+//	int max=0;
+//	max=Max(10,20);
+//	printf("%d\n",max);
+//
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int num1=10;
+//	int num2=20;
+//	if(num1>num2)
+//		printf("%d\n",num1);
+//	else
+//		printf("%d\n",num2);
+//
+//return 0;
+//}
+//extern int Add(int, int);
+//#include<stdio.h>
+//int main()
+//{
+//	int a=10;
+//	int b=20;
+//	int sum=0;
+//	sum=Add(a,b);
+//	printf("%d\n",sum);
+//
+//
+//return 0;
+//}
