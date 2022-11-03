b0917a01a02f9f2107fc7f00013d24cd87c4a5ac
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2022-11-03 21:06:46 +0800

    Add files via upload

---
+++ 11.3.cpp
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	system("color F3");
+//	int year;
+//	scanf("%d", &year);
+//	if (year%4 !=0)
+//		printf("不是闰年");
+//	else
+//		printf("是闰年");
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	system("color F3");
+//	int year;
+//	scanf("%d", &year);
+//	if (year % 4 != 0)
+//		printf("不是闰年");
+//	else
+//		printf("是闰年");
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	int day1,day2;
+//	scanf("%d", &day1);
+//	day2 = day1 + 2;
+//	printf("后天是");
+//	switch (day2)
+//	{
+//	case 1:
+//		printf("星期一");
+//		break;
+//	case 2:
+//		printf("星期二");
+//		break;
+//	case 3:
+//		printf("星期三");
+//		break;
+//	case 4:
+//		printf("星期四");
+//		break;
+//	case 5:
+//		printf("星期五");
+//		break;
+//	case 6:
+//		printf("星期六");
+//		break;
+//	case 7:
+//		printf("星期天");
+//		break;
+//	default:
+//		printf("day<=7\n");
+//		break;
+//	}
+//
+//	return 0;
+//}
