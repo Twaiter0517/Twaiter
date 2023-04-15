31731de8c765fcbc5901b717e132d4ead4119f18
Twaiter0517 <115345209+Twaiter0517@users.noreply.github.com>
2023-04-15 23:31:09 +0800

    Add files via upload

---
+++ 4.15.cpp
@@ -0,0 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//	int day=0;
+//	
+//		scanf("%d",&day);
+//		switch(day)
+//		{
+//		case 1:
+//		case 2:
+//		case 3:
+//		case 4:
+//		case 5:
+//			printf("workday\n");
+//			break;
+//		case 6:
+//		case 7:
+//			printf("weekday\n");
+//			break;
+//		default:
+//			printf("missing\n");
+//		}
+//
+//return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int ch=0;
+//	while((ch=getchar())!=EOF)
+//	{
+//	//printf("%d\n",ch);
+//		putchar(ch);
+//	}
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("become a college student\n");
+//	int line=0;
+//	for(line=0;line<2000;line++)
+//	{
+//	printf("敲代码~~~\n");
+//	
+//	
+//	
+//	}
+//	printf("get the offer");
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	
+//	int i=0;
+//	do
+//	{
+//	printf("%d\n",i);
+//	i++;
+//
+//	}
+//	while(i<=100);
+//	
+//return 0;
+//}
+//#include<stdlib.h>
+//#include<string.h>
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	system("color F0");
+//	char arr1[]="welcome to bit!!!!!!!";
+//	char arr2[]="#####################";
+//	int left=0;
+//	int right=sizeof(arr1)/sizeof(arr1[0])-2;
+//	//int right=strlen(arr1)-1;
+//	while(left<=right)
+//	{
+//	arr2[left]=arr1[left];
+//	arr2[right]=arr1[right];
+//	printf("%s\n",arr2);
+//	Sleep(500);//(休息半秒)（需要stdlib.h）
+//	system("cls");//(清空屏幕)(需要windows.h)
+//	left++;
+//	right--;
+//
+//	}
+//	printf("%s\n",arr2);
+//return 0;
+//}
